e824e7c86ece921fb9643be5ad81e946a240dc1e
Karthikeyan A <whonoah.exe@gmail.com>
2026-04-21 16:31:40 +0530

    feat: set up fonts & variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
-import { Geist, Geist_Mono, Inter, JetBrains_Mono } from "next/font/google"
+import { JetBrains_Mono, Lato } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-const jetbrainsMonoHeading = JetBrains_Mono({subsets:['latin'],variable:'--font-heading'});
-
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
-
-const fontMono = Geist_Mono({
+const lato = Lato({
   subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-serif",
+})
+
+const fontMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["400"],
   variable: "--font-mono",
 })
 
@@ -22,9 +25,9 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable, jetbrainsMonoHeading.variable)}
+      className={cn("antialiased", lato.variable, fontMono.variable)}
     >
-      <body>
+      <body className="font-serif">
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
