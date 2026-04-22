5b3a670cb3b102397393729656c8dd562a432936
Karthikeyan A <whonoah.exe@gmail.com>
2026-04-22 22:41:27 +0530

    feat: problems section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { ArrowUpRight } from "lucide-react"
 
 export default function Page() {
   return (
     <div className="px-5">
-      <div className="mt-20 flex h-200 w-full rounded-md bg-pink-50 py-14">
-        <div className="flex max-w-200 flex-col items-center justify-between pl-40">
-          <div className="flex flex-col justify-between gap-3">
+      {/* Hero section */}
+      <div className="mt-20 flex h-200 w-full rounded-md bg-pink-50 py-30">
+        <div className="flex max-w-200 flex-col justify-between pl-30">
+          <div className="flex max-w-115 flex-col gap-3">
             <p className="pr-8 font-sans text-[64px] leading-16 font-black text-neutral-950 uppercase">
               Creativity at Volume.
             </p>
@@ -30,6 +32,43 @@ export default function Page() {
           </div>
         </div>
       </div>
+
+      {/* Problems section */}
+      <Tabs defaultValue="bottleneck" className="my-20">
+        <TabsList>
+          <TabsTrigger value="bottleneck">Bottleneck</TabsTrigger>
+          <TabsTrigger value="lag">Lag</TabsTrigger>
+          <TabsTrigger value="burnout">Burnout</TabsTrigger>
+          <TabsTrigger value="risk">Risk</TabsTrigger>
+        </TabsList>
+        <TabsContent
+          value="bottleneck"
+          className="flex h-200 w-full flex-none justify-between rounded-md bg-lime-200"
+        >
+          <div className="flex h-full flex-col justify-between p-30">
+            <div className="flex max-w-115 flex-col gap-6">
+              <p className="type-heading-1 text-lime-950">
+                Scaling ad spend is easy. Scaling creatives?
+              </p>
+              <p className="type-heading-3 text-accent-foreground">
+                Not so much.
+              </p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <p className="type-monospaced text-primary">Sound familiar?</p>
+              <p className="type-heading-4 max-w-170 text-accent-foreground">
+                Your ad platforms move faster than your team can produce.
+                Winning ads stale out before you can replace them.
+              </p>
+            </div>
+          </div>
+          <div className="flex h-full w-205 items-center justify-center rounded-md bg-card text-card-foreground">
+            <p className="type-monospaced text-foreground">
+              (replace with actual video embed)
+            </p>
+          </div>
+        </TabsContent>
+      </Tabs>
     </div>
   )
 }
